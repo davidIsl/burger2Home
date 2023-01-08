53b91cbcdb5e174931cdf564d3bb3440f499b32d
davidIsl <101212305+davidIsl@users.noreply.github.com>
2023-01-08 20:48:31 +0100

    feat: stripe

--- components/basket/stripe.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug"></template>
-<script lang="ts">
-import { Vue } from 'nuxt-property-decorator';
-// import Stripe from 'stripe';
-
-export default class StripePayments extends Vue {
-  // stripe: Stripe;
-}
-</script>
